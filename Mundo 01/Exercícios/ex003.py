eb01c0f63ef6928507484b5672c2041fc46fc257
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-05 13:36:11 -0300

    ex003

---
+++ Mundo 01/Exercícios/ex003.py
@@ -0,0 +1,5 @@
+# Desafio 003 feito pelo Professor Guanabara, mas eu fiz na pasta de Desafios.
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
