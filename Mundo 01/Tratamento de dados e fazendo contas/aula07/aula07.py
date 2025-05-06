e2ce9f8f098308c61f97bda11b1fcf1b8e97dffc
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-05 23:17:07 -0300

    finish aula 07

---
+++ Mundo 01/Tratamento de dados e fazendo contas/aula07/aula07.py
@@ -0,0 +1,12 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Um outro valor: '))
+sm = n1 + n2
+mut = n1 * n2
+div = n1 / n2
+divi = n1 // n2
+es = n1 ** n2
+print('A soma vale {}, o produto é {} e a divisão {:.3}'.format(sm, mut, div), end=' >>> ')
+print('Divisão Inteira: {} e Potência: {}'.format(divi, es))
+
+#name = input('Qual é seu nome? ')
+#print('Prazer em te conhecer {:-^20}!'.format(name))
