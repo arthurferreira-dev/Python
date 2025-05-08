3bd6169b062d8411249a9877d0815e43369866e7
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-08 17:15:18 -0300

    finish exercise 010

--- Mundo 01/Desafios/desafio10.py
+++ Mundo 01/Desafios/desafio10.py
@@ -1,3 +1,9 @@
-print('ISSO É FICCIONAL NÃO É PARA COLOCAR REALMENTE O VALOR DO SEU DINHEIRO!')
-print('CONSIDERE: US$1.00 = R$5.71')
-cart = int(input('Quantos Reais você tem? '))
+print('ISSO É FICCIONAL NÃO É PARA COLOCAR REALMENTE O VALOR DO DINHEIRO QUE VOCÊ TEM!')
+print('CONSIDERE: US$1.00 = R$5.67 | €1.00 = R$6.36 | ¥1.00 = R$0.039')
+real = float(input('Quantos Reais você tem? R$'))
+dol = real / 5.67
+euro = real / 6.36
+iene = real / 0.039
+print('Você consegue comprar US${:.2f} Dólar(es)'.format(dol))
+print('Você consegue comprar €{:.2f}'.format(euro))
+print('Você consegue comprar ¥{:.2f}'.format(iene))
