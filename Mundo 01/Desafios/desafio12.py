7bf939435233e40f1215e2b777c86479b172b9da
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-08 17:44:02 -0300

    finish exercise 012

--- Mundo 01/Desafios/desafio12.py
+++ Mundo 01/Desafios/desafio12.py
@@ -1,5 +1,4 @@
 print('Coloque um . ao inves da , POR FAVOR! (você pode colocar um número inteiro)')
-prc = float(input('Digite um preço: '))
-des = prc * 0.05
-fim = prc - 1.25
-print('O preço do produto foi de {}R$ para {}R$ com os 5% de Desconto.'.format(prc, fim))
+prc = float(input('Digite um preço: R$'))
+des = prc - (prc * 5 / 100)
+print('O preço do produto foi de R${} para R${} com os 5% de Desconto.'.format(prc, des))
