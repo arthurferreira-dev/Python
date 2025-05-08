298f12ae6e61f05b37d5e74a0700ec3bd41f5444
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-08 17:29:37 -0300

    finish exercise 011

--- Mundo 01/Desafios/desafio11.py
+++ Mundo 01/Desafios/desafio11.py
@@ -1,5 +1,5 @@
-alt = int(input('Digite um número: '))
-lar = int(input('Digite outro número: '))
+alt = float(input('Digite um número: '))
+lar = float(input('Digite outro número: '))
 area = alt * lar
 tinta = area / 2
-print('A área da sua parede é {}m² e você precisa de {}L de tinta.'.format(area, tinta))
+print('A área da sua parede é {}m² e você precisa de {:.2f}L de tinta.'.format(area, tinta))
