0767853ac0dcbe41f5ad68bd1171f68ad5eb9064
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-08 16:49:36 -0300

    finish exercise 08

--- Mundo 01/Desafios/desafio08.py
+++ Mundo 01/Desafios/desafio08.py
@@ -1,4 +1,4 @@
 metros = int(input('Digite um número: '))
 cm = metros * 100
 mm = metros * 1000
-print('Seus metros são {}, seu centímetros são {} e seus milímetros são {}.'.format(metros, cm, mm))
+print('{}m \n{}cm \n{}mm'.format(metros, cm, mm))
