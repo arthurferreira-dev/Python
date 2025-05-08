8c096687c0943485865a3680357995e14c7ff6c1
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-07 21:26:09 -0300

    finish exercise 006

--- Mundo 01/Desafios/desafio06.py
+++ Mundo 01/Desafios/desafio06.py
@@ -2,4 +2,4 @@ num = int(input('Digite um número: '))
 dobro = num * 2
 triplo = num * 3
 raiz = num ** (1/2)
-print('Seu número é {}, seu dobro é {}, seu triplo é {} e sua raiz quadrada é {}'.format(num, dobro, triplo, raiz))
+print('Seu número é {}, seu dobro é {}, seu triplo é {} e sua raiz quadrada é {:.2f}'.format(num, dobro, triplo, raiz))
