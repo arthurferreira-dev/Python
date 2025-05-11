c07ea663893b6c910d6e54123e877e1779b75d41
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-11 18:46:46 -0300

    finish ex014

--- Mundo 01/Desafios/desafio14.py
+++ Mundo 01/Desafios/desafio14.py
@@ -1,3 +1,3 @@
 temp = float(input('Informe uma temperatura em °C: '))
-fah = (temp * 9) / 5 + 32
+fah = temp * 9 / 5 + 32
 print('A temperatura de {:.1f}°C corresponde a {:.1f}°F!'.format(temp, fah))
