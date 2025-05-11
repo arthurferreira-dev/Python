9184305076b95b912e1ca88159bacc1765d35961
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-11 16:02:54 -0300

    finish ex013

--- Mundo 01/Desafios/desafio13.py
+++ Mundo 01/Desafios/desafio13.py
@@ -1,4 +1,4 @@
 print('Coloque um . ao inves da , POR FAVOR! (você pode colocar número inteiro)')
 sal = float(input('Coloque um salário: '))
 up = sal * 1.15
-print('Seu salário foi de {} para {} com os 15% de aumento.'.format(sal, up))
+print('Seu salário foi de {:.3f} para {:.3f} com os 15% de aumento.'.format(sal, up))
