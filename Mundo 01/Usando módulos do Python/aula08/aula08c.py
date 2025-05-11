bab5b27a71eca21e3df47f383907c293d4390845
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-11 20:00:41 -0300

    finish aula08

---
+++ Mundo 01/Usando módulos do Python/aula08/aula08c.py
@@ -0,0 +1,5 @@
+import emoji
+
+print(emoji.emojize("Olá mundo :globe_with_meridians:"))
+print(emoji.emojize("Python é legal :red_heart:", variant="emoji_type"))
+print(emoji.emojize("Bora programar :desktop_computer:!"))
